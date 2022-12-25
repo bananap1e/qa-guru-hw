bb521013ac90f179a85cffbd2f15702936925a45
Nikita <sourbeanpie@gmail.com>
2022-12-25 23:19:50 +0300

    refactoring

--- src/test/java/homework/Junit5ExampleAtSoftAssertions.java
+++ src/test/java/homework/Junit5ExampleAtSoftAssertions.java
@@ -2,7 +2,7 @@ package homework;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
@@ -12,13 +12,17 @@ public class Junit5ExampleAtSoftAssertions {
     void ShouldExtendsJUnit5ExampleAtSoftAssertions() {
         // Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
+
         //  Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
+
         // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $(".js-wiki-more-pages-link").click();
         $$("#wiki-pages-box").findBy(visible).shouldHave(text("SoftAssertions"));
+
         // Откройте страницу SoftAssertions,
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
         // проверьте что внутри есть пример кода для JUnit5
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
